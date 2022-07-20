0285711117377b2ebed86639c94ea00789b5fdfd
Erdem Erdoğan <erdem.erdogan.cm@outlook.com>
2022-07-20 16:40:43 +0300

    Character Movement and Snow Controller
    Its a Game Trial, Subjects are Character Movement and Snow Controller using with C# & Unity.

---
+++ ExpressGems/Assets/RPGPP_LT/Materials/SnowController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowController : MonoBehaviour
+{
+    [Range(0.01f, 0.1f)]
+    public float snowCoverSpeed;
+    float value = 0;
+
+    private void Start()
+    {
+        Shader.SetGlobalFloat("_SnowLevel", 0f);
+    }
+
+
+    private void Update()
+    {
+        if (value < 1f)
+        {
+            Shader.SetGlobalFloat("_SnowLevel", value);
+            value += Time.deltaTime * snowCoverSpeed;
+        }
+    }
+}
